552325428bdc59efb0d8c56293a64fa8fccc3e4f
João Victor Vieira Marcondes <166854246+joaobgoode@users.noreply.github.com>
2025-11-10 10:10:39 -0300

    Projeto inicial

---
+++ MapaUnifor/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  MapaUnifor
+//
+//  Created by Turma01-8 on 10/11/25.
+//
+
+import SwiftUI
+
+
+struct Location {
+    var latitude: Double
+    var longitude: Double
+    var andar: Int
+    var descricao: String
+}
+
+struct Bloco {
+    var location: Location
+    var nome: String
+}
+
+struct Banheiro {
+    var bloco: Bloco
+    var lcoatichon: Location
+    var sexo: Character
+    var acessivel: Bool
+}
+
+enum Categoria {
+    case laboratorio
+    case centroacademico
+    case secretariaacademica
+    case pontocarrinho
+    // TODO adicionar mais
+}
+
+struct LocalizacaoDeInteresse {
+    var bloco: Bloco
+    var locoation: Location
+    var nome: String
+    var categoria: Categoria
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapaUnifor/MapaUniforApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapaUniforApp.swift
+//  MapaUnifor
+//
+//  Created by Turma01-8 on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MapaUniforApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
