147f4c31840df5074fcb69821b020cf61f42025f
gabEsmeraldo <gabrielcesmeraldo@gmail.com>
2025-11-11 11:23:15 -0300

    added missing files

---
+++ MapaUnifor/Contato.swift
@@ -0,0 +1,8 @@
+//
+//  Contato.swift
+//  MapaUnifor
+//
+//  Created by Turma01-7 on 11/11/25.
+//
+
+import Foundation
---
+++ MapaUnifor/ContatosView.swift
@@ -0,0 +1,18 @@
+//
+//  ContatosView.swift
+//  MapaUnifor
+//
+//  Created by Turma01-7 on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContatosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContatosView()
+}
