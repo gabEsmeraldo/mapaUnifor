e3e4369b97a2a8b4b934441594cac59b106154c6
gabEsmeraldo <gabrielcesmeraldo@gmail.com>
2025-11-11 10:48:08 -0300

    Changed structs a bit, needing review

--- MapaUnifor/Structs.swift
+++ MapaUnifor/Structs.swift
@@ -6,37 +6,50 @@
 //
 
 import Foundation
-
+ //Analisar como organizar o banco baseado nas structs, também checar se seria possível usar somente um, ou necessario usar um por struct.
+//Checar se realmente a implementação utilizando id é mais facil do que com os objetos
 struct Location {
+    var id: Int
     var latitude: Double
     var longitude: Double
-    var andar: Int
-    var descricao: String
+    var andar: Int?
+    var descricao: String?
 }
 
 struct Bloco {
-    var location: Location
+    var blocoID: Int
+    var locationID: Int
+//    var location: Location
     var nome: String
 }
 
 struct Banheiro {
-    var bloco: Bloco
-    var location: Location
+    var blocoID: Int
+    var locationID: Int
+//    var bloco: Bloco
+//    var location: Location
     var sexo: Character
     var acessivel: Bool
 }
 
 enum Categoria {
     case laboratorio
-    case centroacademico
-    case secretariaacademica
-    case pontocarrinho
+    case centroAcademico
+    case secretariaAcademica
+    case pontoCarrinho
+    case lanchonete
+    case vendinha
+    case pontoInstitucional
     // TODO adicionar mais
 }
 
 struct LocalizacaoDeInteresse {
-    var bloco: Bloco
-    var location: Location
+    var blocoID: Int
+    var locationID: Int
+//    var bloco: Bloco
+//    var location: Location
     var nome: String
     var categoria: Categoria
 }
+
+
