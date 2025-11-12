452e53e551cccfb7b49efeccc5cfff5970c7ff2f
Matheus b <matheus1braga@hotmail.com>
2025-11-12 10:45:33 -0300

    Codigo DetalhesSheetView

--- MapaUnifor/DetalhesSheetView.swift
+++ MapaUnifor/DetalhesSheetView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct DetalhesSheetView: View {
+    let local: LocalizacaoDeInteresse
+     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.azul)
+                .ignoresSafeArea()
+            VStack (alignment: .center){
+                AsyncImage(url: URL(string: "https://example.com/your_image.jpg"))
+                    .frame(width: 300, height: 400)
+                Text(local.nome)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Text("Bloco M - " + String(local.location?.andar ?? 0) + "º andar")
+                    .font(.headline)
+                Text(local.categoria.string)
+                    .font(.subheadline)
+                Text(local.location?.descricao ?? "")
+                    .frame(width: 300)
+                    .padding(.top)
+                
+            }
+            .padding()
+            .background(.branco)
+            .cornerRadius(8)
+        }
     }
 }
 
 #Preview {
-    DetalhesSheetView()
+    DetalhesSheetView(local: LocalizacaoDeInteresse(id: 0, blocoID: 0, locationID: 0, nome: "Vortex", categoria: Categoria.laboratorio, location: Location(id: 0, latitude: 0, longitude: 0, descricao: "O Vortex desenvolve projetos de inovação tecnologica e capacita alunos para atuarem nessa área.")))
 }
