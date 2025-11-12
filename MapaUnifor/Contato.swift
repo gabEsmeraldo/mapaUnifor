aa292064bfea4d2a5930ee96df4d502823976278
Matheus b <matheus1braga@hotmail.com>
2025-11-12 08:25:09 -0300

    Codigo para struct Contatos e view ContatosView

--- MapaUnifor/Contato.swift
+++ MapaUnifor/Contato.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct Contato: Hashable {
+    let nome: String
+    let tel: String
+    let whats: String?
+    let email: String?
+}
+
+//numbersOnlyString = originalString.filter { $0.isWholeNumber }
+
+let contatos: [Contato] = [
+    Contato(nome: "Segurança", tel: "(85) 3477-3134", whats: "(85) 99109-6657", email: nil),
+    Contato(nome: "Central de Atendimento", tel: "(85) 3477-3000", whats: "(85) 99246-6625", email: "sucessodoaluno@unifor.br"),
+    Contato(nome: "Biblioteca Central", tel: "(85) 3477-3257", whats: nil, email: "bib@unifor.br"),
+    Contato(nome: "Central de Carreiras e Egressos", tel: "(85) 3477-3142", whats: "(85) 99249-5956", email: "estagio@unifor.br"),
+    Contato(nome: "Núcleo de Atenção Médica Integrada (NAMI)", tel: "(85) 99200-7069", whats: nil, email: "nami@unifor.br"),
+    Contato(nome: "Programa de Apoio Psicopedagógico (PAP)", tel: "(85) 3477-3399", whats: "(85) 99250-7530", email: "pap@unifor.br"),
+    Contato(nome: "Núcleo de Tecnologias Educacionais (NTE)", tel: "(85) 3477-3479", whats: nil, email: "curso.ead@unifor.br"),
+    Contato(nome: "Diretoria de Tecnologia (DTec)", tel: "(85) 3477-3000", whats: nil, email: nil),
+    Contato(nome: "Núcleo de Apoio Contábil e Fiscal (NAF)", tel: "(85) 3477-3193", whats: nil, email: "naf@unifor.br"),
+    Contato(nome: "Escritório de Prática Jurídica (EPJ)", tel: "(85) 3477-3332", whats: nil, email: nil),
+    Contato(nome: "Núcleo de Estratégias Internacionais", tel: "(85) 3477-3834", whats: nil, email: "nei@unifor.br"),
+    Contato(nome: "Escritório EducationUSA/UNIFOR", tel: "(85) 3477-3481", whats: nil, email: "educationusa@unifor.br"),
+    Contato(nome: "Vice-Reitoria de Pesquisa (VRP)", tel: "(85) 3477-3889", whats: nil, email: "dpdi@unifor.br"),
+    Contato(nome: "Vice-Reitoria de Extensão e Comunidade Universitária", tel: "(85) 3477-3311", whats: nil, email: "extensao@unifor.br"),
+    Contato(nome: "Diretório Central dos Estudantes (DCE)", tel: "(85) 3185-3477", whats: nil, email: nil),
+]
+
+
--- MapaUnifor/ContatosView.swift
+++ MapaUnifor/ContatosView.swift
@@ -9,10 +9,53 @@ import SwiftUI
 
 struct ContatosView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ForEach(contatos, id: \.self) { contato in
+                contatoRow(contato: contato)
+            }
+        }
+    }
+    
+    func contatoRow (contato: Contato) -> some View {
+        HStack{
+            VStack(alignment: .leading){
+                Text(contato.nome)
+                    .bold()
+                VStack(alignment: .leading){
+                    if (contato.email != nil) {
+                        HStack{
+                            Image(systemName: "envelope")
+                            Text(contato.email!)
+                            
+                        }
+                    }
+                    if (contato.whats != nil) {
+                        HStack{
+                            Image(systemName: "message")
+                            Text(contato.whats!)
+                            
+                        }
+                    }
+                }
+                .font(.subheadline)
+            }
+            .foregroundStyle(.white)
+            Spacer()
+            Link(destination: URL(string: "tel:\(contato.tel.filter { $0.isWholeNumber })")!) {
+                Image(systemName: "phone")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+            
+        }
+        .padding(.horizontal)
+        .frame(width: 350, height: 80)
+        .background(.blue)
+        .cornerRadius(8)
     }
 }
 
 #Preview {
     ContatosView()
 }
+
