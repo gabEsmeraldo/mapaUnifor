a61d5329dbec7ee9ddf86420f69a439340877b6f
gabEsmeraldo <gabrielcesmeraldo@gmail.com>
2025-11-12 10:39:45 -0300

    Added DetalhesSheetView to project

---
+++ MapaUnifor/DetalhesSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  DetalhesSheetView.swift
+//  MapaUnifor
+//
+//  Created by Turma01-7 on 12/11/25.
+//
+
+import SwiftUI
+
+struct DetalhesSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetalhesSheetView()
+}
