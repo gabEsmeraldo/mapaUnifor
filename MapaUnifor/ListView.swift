1b6988f05e39d4d1d4b7946d1ce69e0d3bbfe6c1
Matheus b <matheus1braga@hotmail.com>
2025-11-17 12:18:31 -0300

    ListView

--- MapaUnifor/ListView.swift
+++ MapaUnifor/ListView.swift
@@ -8,18 +8,17 @@
 import SwiftUI
 
 struct ListView: View {
-    
     @EnvironmentObject var coordinator: MapCoordinator
     @State var showingBloco: Bloco? = nil
-    
+     
     var blocos = [
-        Bloco(id: 1, locationID: 1, nome: "K", location: Location(
+        Bloco(id: 1, positionID: 1, nome: "K", location: Location(
             id: 1, latitude: -3.7698, longitude: -38.4788
         )),
-        Bloco(id: 2, locationID: 2, nome: "J", location: Location(
+        Bloco(id: 2, positionID: 2, nome: "J", location: Location(
             id: 2, latitude: -3.7705, longitude: -38.4792
         )),
-        Bloco(id: 3, locationID: 3, nome: "H", location: Location(
+        Bloco(id: 3, positionID: 3, nome: "H", location: Location(
             id: 3, latitude: -3.7710, longitude: -38.4782
         )),
     ]
@@ -103,7 +102,7 @@ struct ListView: View {
                     }
                 }
             }
-            
+               
 
         }
     }
